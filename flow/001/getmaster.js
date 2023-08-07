0026625b683e9b3cdab201e87a918030aeb2477b
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2023-08-07 09:39:10 +0700

    .

--- flow/001/getmaster.js
+++ flow/001/getmaster.js
@@ -55,6 +55,10 @@ router.post('/GETINSset', async (req, res) => {
     findPO = await mongodb.find(MAIN_DATA, MAIN, { "PO": input['PO'] });
 
 
+    if(findcp.length === 0 ){
+      findcp = await mongodb.find(PATTERN, PATTERN_01, { "FG": input['CP'] });
+    }
+
   }
   if (findcp.length > 0 && findPO.length === 0) {
     if (findcp[0]['FINAL'] !== undefined && findcp[0]['FINAL'].length > 0) {
