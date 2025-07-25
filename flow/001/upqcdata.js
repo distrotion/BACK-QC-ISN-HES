9a0094e92349e52c3b60c42bcb0a1611532afc39
Thai Parkerizing <administrator@syn-172-101-034-209.res.spectrum.com>
2025-07-25 15:55:25 +0700

    .

--- flow/001/upqcdata.js
+++ flow/001/upqcdata.js
@@ -3,15 +3,73 @@ const router = express.Router();
 var mongodb = require('../../function/mongodb');
 
 
-router.post('/upqcdata',async (req,res) => {
-    //-------------------------------------
-    console.log('--upqcdata--');
-    console.log(req.body);
-    //-------------------------------------
-    
-    
-    //-------------------------------------
-      res.json(output);
+router.post('/upqcdata', async (req, res) => {
+  //-------------------------------------
+  console.log('--upqcdata--');
+  console.log(req.body);
+  //-------------------------------------
+
+
+  //-------------------------------------
+  res.json(output);
+});
+
+router.post('/rugamplitude', async (req, res) => {
+  //-------------------------------------
+  console.log('--rugamplitude--');
+  console.log(req.body);
+  let input = req.body;
+  let output = '';
+  //-------------------------------------
+  let memeP = ''
+  let memeN = ''
+  let coutP = 0
+  let coutN = 0
+
+  if (input['data'] != undefined && input['limit'] != undefined) {
+
+
+
+    for (let i = 0; i < input['data'].length; i++) {
+
+
+      // console.log(parseFloat(input['data'][i]['Z']));
+
+
+      if (parseFloat(input['data'][i]['Z']) >= parseFloat(`${input['limit']}`)) {
+        if (memeP === '') {
+          memeP = 'alresdy'
+          coutP++
+        }
+      } else {
+        memeP = ''
+      }
+
+      if (parseFloat(input['data'][i]['Z']) <= -parseFloat(`${input['limit']}`)) {
+        if (memeN === '') {
+          memeN = 'alresdy'
+          coutN++
+        }
+      } else {
+        memeN = ''
+      }
+
+
+
+
+
+
+    }
+
+    output = { "P": coutP, "N": coutN, "CP": coutP+coutN, }
+
+  }
+
+
+
+
+  //-------------------------------------
+  res.json(output);
 });
 
 
